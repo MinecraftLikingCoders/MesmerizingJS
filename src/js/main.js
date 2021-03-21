ba4a7bb4f0f433a8f9108bcd56ab7f62c870c54a
mason <masondwithers@gmail.com>
2021-03-21 15:55:19 -0400

    Add mesmerizing Canvas

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,36 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 80, 80);
-var kni = document.getElementById("knight");
-ctx.drawImage(img, 10, 10);
+var x = canvas.width/2;
+var y = canvas.height/2;
+var dx = Math.floor(Math.random() * 10);
+var dy = Math.floor(Math.random() * 10);
+var ballRadius = Math.floor(Math.random() * 15);
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+    x += dx;
+    y += dy;
+}
+for(var i = 0; i<10; i++)
+    
+
+    setInterval(draw, 10);
+setTimeout(function(){window.location.reload(1);}, 1000);
+     
